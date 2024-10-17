93d2831ec5d08481f86ef01b506f4ce20df4e66d
LeNhatThien198 <lenhatthien198@gmail.com>
2024-10-17 10:53:25 +0700

    UPBUOI4

---
+++ DTH225771.BuoiTH04/Buoi04/Bai1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Bai1
+{
+    partial class frmMangSoNguyen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnSXGiam = new System.Windows.Forms.Button();
+            this.btnSXTang = new System.Windows.Forms.Button();
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.lblKQ = new System.Windows.Forms.Label();
+            this.btnSoNguyenTo = new System.Windows.Forms.Button();
+            this.btnTBMang = new System.Windows.Forms.Button();
+            this.btnUCLN = new System.Windows.Forms.Button();
+            this.btnDemSoLe = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.btnSHH = new System.Windows.Forms.Button();
+            this.btnDemSoChan = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhap.Location = new System.Drawing.Point(9, 12);
+            this.btnNhap.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(160, 39);
+            this.btnNhap.TabIndex = 0;
+            this.btnNhap.Text = "Nhập 1 Phần Tử";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // btnIn
+            // 
+            this.btnIn.Enabled = false;
+            this.btnIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIn.Location = new System.Drawing.Point(317, 12);
+            this.btnIn.Margin = new System.Windows.Forms.Padding(4);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(107, 39);
+            this.btnIn.TabIndex = 1;
+            this.btnIn.Text = "In Mảng";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(432, 12);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(4);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(133, 39);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(573, 12);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(133, 39);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnSXGiam
+            // 
+            this.btnSXGiam.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSXGiam.Location = new System.Drawing.Point(573, 59);
+            this.btnSXGiam.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSXGiam.Name = "btnSXGiam";
+            this.btnSXGiam.Size = new System.Drawing.Size(133, 39);
+            this.btnSXGiam.TabIndex = 4;
+            this.btnSXGiam.Text = "Sắp Xếp Giảm";
+            this.btnSXGiam.UseVisualStyleBackColor = true;
+            this.btnSXGiam.Click += new System.EventHandler(this.btnSXGiam_Click);
+            // 
+            // btnSXTang
+            // 
+            this.btnSXTang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSXTang.Location = new System.Drawing.Point(573, 106);
+            this.btnSXTang.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSXTang.Name = "btnSXTang";
+            this.btnSXTang.Size = new System.Drawing.Size(133, 39);
+            this.btnSXTang.TabIndex = 5;
+            this.btnSXTang.Text = "Sắp Xếp Tăng";
+            this.btnSXTang.UseVisualStyleBackColor = true;
+            this.btnSXTang.Click += new System.EventHandler(this.btnSXTang_Click);
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Location = new System.Drawing.Point(177, 16);
+            this.txtNhap.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNhap.Multiline = true;
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(132, 31);
+            this.txtNhap.TabIndex = 15;
+            // 
+            // lblKQ
+            // 
+            this.lblKQ.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblKQ.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblKQ.Location = new System.Drawing.Point(13, 59);
+            this.lblKQ.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(552, 86);
+            this.lblKQ.TabIndex = 16;
+            this.lblKQ.Click += new System.EventHandler(this.lblKQ_Click);
+            // 
+            // btnSoNguyenTo
+            // 
+            this.btnSoNguyenTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSoNguyenTo.Location = new System.Drawing.Point(291, 153);
+            this.btnSoNguyenTo.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSoNguyenTo.Name = "btnSoNguyenTo";
+            this.btnSoNguyenTo.Size = new System.Drawing.Size(133, 39);
+            this.btnSoNguyenTo.TabIndex = 17;
+            this.btnSoNguyenTo.Text = "Số Nguyên Tố";
+            this.btnSoNguyenTo.UseVisualStyleBackColor = true;
+            this.btnSoNguyenTo.Click += new System.EventHandler(this.btnSoNguyenTo_Click);
+            // 
+            // btnTBMang
+            // 
+            this.btnTBMang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTBMang.Location = new System.Drawing.Point(248, 200);
+            this.btnTBMang.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTBMang.Name = "btnTBMang";
+            this.btnTBMang.Size = new System.Drawing.Size(175, 39);
+            this.btnTBMang.TabIndex = 18;
+            this.btnTBMang.Text = "Trung Bình Mảng";
+            this.btnTBMang.UseVisualStyleBackColor = true;
+            this.btnTBMang.Click += new System.EventHandler(this.btnTBMang_Click);
+            // 
+            // btnUCLN
+            // 
+            this.btnUCLN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUCLN.Location = new System.Drawing.Point(9, 200);
+            this.btnUCLN.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUCLN.Name = "btnUCLN";
+            this.btnUCLN.Size = new System.Drawing.Size(231, 39);
+            this.btnUCLN.TabIndex = 19;
+            this.btnUCLN.Text = "UCLN 2 Phần Tử Đầu Tiên";
+            this.btnUCLN.UseVisualStyleBackColor = true;
+            this.btnUCLN.Click += new System.EventHandler(this.btnUCLN_Click);
+            // 
+            // btnDemSoLe
+            // 
+            this.btnDemSoLe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDemSoLe.Location = new System.Drawing.Point(150, 153);
+            this.btnDemSoLe.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDemSoLe.Name = "btnDemSoLe";
+            this.btnDemSoLe.Size = new System.Drawing.Size(133, 39);
+            this.btnDemSoLe.TabIndex = 25;
+            this.btnDemSoLe.Text = "Đếm Số Lẻ";
+            this.btnDemSoLe.UseVisualStyleBackColor = true;
+            this.btnDemSoLe.Click += new System.EventHandler(this.btnDemSoLe_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMin.Location = new System.Drawing.Point(432, 200);
+            this.btnMin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(133, 39);
+            this.btnMin.TabIndex = 20;
+            this.btnMin.Text = "Số Nhỏ Nhất";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnMax
+            // 
+            this.btnMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMax.Location = new System.Drawing.Point(573, 200);
+            this.btnMax.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(133, 39);
+            this.btnMax.TabIndex = 24;
+            this.btnMax.Text = "Số Lớn Nhất";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // btnSHH
+            // 
+            this.btnSHH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSHH.Location = new System.Drawing.Point(432, 153);
+            this.btnSHH.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSHH.Name = "btnSHH";
+            this.btnSHH.Size = new System.Drawing.Size(133, 39);
+            this.btnSHH.TabIndex = 21;
+            this.btnSHH.Text = "Số Hoàn Hảo";
+            this.btnSHH.UseVisualStyleBackColor = true;
+            this.btnSHH.Click += new System.EventHandler(this.btnSHH_Click);
+            // 
+            // btnDemSoChan
+            // 
+            this.btnDemSoChan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDemSoChan.Location = new System.Drawing.Point(9, 153);
+            this.btnDemSoChan.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDemSoChan.Name = "btnDemSoChan";
+            this.btnDemSoChan.Size = new System.Drawing.Size(133, 39);
+            this.btnDemSoChan.TabIndex = 23;
+            this.btnDemSoChan.Text = "Đếm Số Chẵn";
+            this.btnDemSoChan.UseVisualStyleBackColor = true;
+            this.btnDemSoChan.Click += new System.EventHandler(this.btnDemSoChan_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.Location = new System.Drawing.Point(573, 153);
+            this.btnSum.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(133, 39);
+            this.btnSum.TabIndex = 22;
+            this.btnSum.Text = "Tổng Mảng";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // frmMangSoNguyen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 251);
+            this.Controls.Add(this.btnSoNguyenTo);
+            this.Controls.Add(this.btnTBMang);
+            this.Controls.Add(this.btnUCLN);
+            this.Controls.Add(this.btnDemSoLe);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnMax);
+            this.Controls.Add(this.btnSHH);
+            this.Controls.Add(this.btnDemSoChan);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.lblKQ);
+            this.Controls.Add(this.txtNhap);
+            this.Controls.Add(this.btnSXTang);
+            this.Controls.Add(this.btnSXGiam);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnIn);
+            this.Controls.Add(this.btnNhap);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMangSoNguyen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mảng Số Nguyên";
+            this.Load += new System.EventHandler(this.frmMangSoNguyen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnSXGiam;
+        private System.Windows.Forms.Button btnSXTang;
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.Label lblKQ;
+        private System.Windows.Forms.Button btnSoNguyenTo;
+        private System.Windows.Forms.Button btnTBMang;
+        private System.Windows.Forms.Button btnUCLN;
+        private System.Windows.Forms.Button btnDemSoLe;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnSHH;
+        private System.Windows.Forms.Button btnDemSoChan;
+        private System.Windows.Forms.Button btnSum;
+    }
+}
+
---
+++ DTH225771.BuoiTH04/Buoi04/Bai1/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1
+{
+    public partial class frmMangSoNguyen : Form
+    {
+        public frmMangSoNguyen()
+        {
+            InitializeComponent();
+        }
+        int[] a = new int[10];
+        int sopt = 0;
+        
+
+        public String InMang()
+        {
+            String chuoi = "";
+            for (int i = 0; i < sopt; i++)
+                chuoi += a[i] + " ";
+            return chuoi;
+        }
+        public int TimMax()
+        {
+            int max = a[0];
+            for (int i = 1;i<sopt;i++)
+            if (a[i] > max)
+                  max = a[i];
+            return max;
+        }
+        public double TrungBinhMang()
+        {
+            int sum = 0;
+            double tb;
+            for (int i = 0; i < sopt; i++)
+                sum += a[i];
+            tb = (double)sum / (double)sopt;
+            return tb;
+        }
+        public void SapXepTang()
+        {
+            Array.Sort(a, 0, sopt);
+        }
+        public void SapXepGiam()
+        {
+            Array.Sort(a, 0, sopt, Comparer<int>.Create((x, y) => y.CompareTo(x)));
+        }
+
+        public int timUCLN(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else b = b - a;
+            }
+            return a;
+        }
+        public Boolean kiemTraSNT(int so)
+        {
+            if (so <= 0)
+                return false;
+            else
+            {
+                for (int i = 2; i <= so / 2; i++)
+                {
+                    if (so % i == 0)
+                        return false;
+                }
+            }
+            return true;
+        }
+        public int demSNT()
+        {
+            int dem = 0;
+            for (int i = 0; i < sopt; i++)
+            {
+                if (kiemTraSNT(a[i]))
+                    dem++;
+            }
+            return dem;
+        }
+        public String inSNT()
+        {
+            String chuoi = "";
+            for (int i = 0; i < sopt; i++)
+            {
+                if (kiemTraSNT(a[i]))
+                    chuoi += a[i] + " ";
+            }
+            return chuoi;
+        }
+        public int DemSoChan()
+        {
+            int dem = 0;
+            for (int i = 0;i < sopt;i++)
+            {
+                if (a[i] % 2 == 0)
+                    dem++;
+            }
+            return dem;
+        }
+        public int DemSoLe()
+        {
+            int dem = 0;
+            for (int i = 0; i < sopt; i++)
+            {
+                if (a[i] % 2 != 0)
+                    dem++;
+            }
+            return dem;
+        }
+        public Boolean kiemTraSHH(int so)
+        {
+            if (so <= 1)
+                return false;
+            int tongUoc = 0;
+
+            for (int i = 1; i <= so / 2; i++)
+            {
+                if (so % i == 0)
+                    tongUoc += i;
+            }
+            return tongUoc == so;
+        }
+        public int demSHH()
+        {
+            int dem = 0;
+            for (int i = 0; i < sopt; i++)
+            {
+                if (kiemTraSHH(a[i]))
+                    dem++;
+            }
+            return dem;
+        }
+        public String inSHH()
+        {
+            String chuoi = "";
+            for (int i = 0; i < sopt; i++)
+            {
+                if (kiemTraSHH(a[i]))
+                    chuoi += a[i] + " ";
+            }
+            return chuoi;
+        }
+        public int TongMang()
+        {
+            int tongMang = 0;
+            for (int i = 0; i < sopt; i++)
+            {
+                tongMang += a[i];
+            }
+            return tongMang;
+        }
+        public int Max()
+        {
+            int max = a[0];
+            for (int i = 1; i < sopt; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            return max;
+        }
+        public int Min()
+        {
+            int min = a[0];
+            for (int i = 1; i < sopt; i++)
+            {
+                if (a[i] > min)
+                {
+                    min = a[i];
+                }
+            }
+            return min;
+        }
+
+
+        private void frmMangSoNguyen_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            if (this.sopt == this.a.Length)
+            {
+                this.txtNhap.Text = "";
+                MessageBox.Show("Mảng đầy", "Thông báo");
+            }
+            else
+            {
+                if (this.txtNhap.Text == "")
+                    MessageBox.Show("Hãy nhập phần tử mảng", "Thông báo");
+                else
+                {
+                    a[sopt] = int.Parse(this.txtNhap.Text);
+                    sopt++;
+                    this.txtNhap.Text += this.txtNhap.Text + " ";
+                    this.txtNhap.Clear();
+                    this.txtNhap.Focus();
+                    if (sopt > 0)
+                        this.btnIn.Enabled = true;
+                }
+            }
+        }
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            //xuat cac phan tu co trong mnag
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+                lblKQ.Text = "Các phần tử trong mảng là: \n\r" + InMang();
+        }
+        private void btnSXTang_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+            {
+                SapXepTang();
+                lblKQ.Text = "Đã sắp xếp mảng tăng dần";
+            }
+        }
+        private void btnSoNguyenTo_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+                lblKQ.Text = "Mảng có " + demSNT().ToString() + " số nguyên tố là " + inSNT();
+        }
+        private void btnUCLN_Click(object sender, EventArgs e)
+        {
+            lblKQ.Text = "Ước chung lớn nhất 2 phần tử đầu " + a[0] + " và " + a[1] + " là " +
+            timUCLN(a[0], a[1]).ToString();
+        }
+
+        private void btnDemSoChan_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+            {
+                DemSoChan();
+                lblKQ.Text = "Mảng có " + DemSoChan().ToString() + " là số chẵn ";
+            }
+        }
+
+        private void btnDemSoLe_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+            {
+                DemSoLe();
+                lblKQ.Text = "Mảng có " + DemSoLe().ToString() + " là số lẻ ";
+            }
+        }
+
+        private void btnSHH_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+                lblKQ.Text = "Mảng có " + demSHH().ToString() + " số hoàn hảo là " + inSHH();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+                lblKQ.Text = "Tổng các phần tử trong mảng là: " + TongMang();
+        }
+
+        private void btnTBMang_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+                lblKQ.Text = "Trung bình các phần tử của mảng là: " + TrungBinhMang();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+                lblKQ.Text = "Số nhỏ nhất trong mảng là: " + Min();
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+                lblKQ.Text = "Số lớn nhất trong mảng là: " + Max();
+        }
+
+        private void btnSXGiam_Click(object sender, EventArgs e)
+        {
+            if (sopt == 0)
+                lblKQ.Text = "Mảng rỗng!";
+            else
+            {
+                SapXepGiam();
+                this.lblKQ.Text = "Đã sắp xếp mảng giảm dần";
+            }
+        }
+
+        private void lblKQ_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
